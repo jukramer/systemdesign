91884745becfb2099a6172c53d37e0f04b28c4cd
jukramer <justas.kramer@gmail.com>
2025-09-03 15:19:36 +0200

    updates

--- calc.py
+++ calc.py
@@ -7,13 +7,33 @@ class Calc:
         pass
     
     def WSMaxApproach(beta): # 1.23 gone for CS23
-        return 1/beta*RHO/2*(V_APP/1.23)**2*CL_MAX_L
+        return 1/beta*RHO_SL/2*(V_APP/1.23)**2*CL_MAX_L
         
     def WSMaxLField(beta):
-        return 1/beta*LANDING_DIST/C_LFL*RHO*CL_MAX_L/2
+        return 1/beta*LANDING_DIST/C_LFL*RHO_SL*CL_MAX_L/2
     
-    def 
-    
+    def CruiseSpeed(beta):
+        pt = P_CR*(1+(GAMMA-1)/GAMMA*MACH_CRUISE**2)**(GAMMA/(GAMMA-1))   
+        Tt = T_CR*(1+(GAMMA-1)/GAMMA*MACH_CRUISE**2)
+        
+        deltat = pt/P_SL 
+        thetat = Tt/T_SL
+        alphat = None
+        
+        if B < 5:
+            if thetat < THETA_BREAK:
+                alphat = deltat
+            elif thetat >= THETA_BREAK:
+                alphat = deltat*(1-2.1*(thetat-THETA_BREAK)/THETA_BREAK)
+        elif B >= 5:
+            if thetat < THETA_BREAK:
+                alphat = deltat*(1-(0.43+0.014*B)*np.sqrt(MACH_CRUISE))
+            elif thetat >= THETA_BREAK:
+                alphat = deltat*(1-(0.43+0.014*B)*np.sqrt(MACH_CRUISE) - 3*(thetat-THETA_BREAK)/(1.5+MACH_CRUISE))
+                
+        
+        
+           
     
             
     
--- parameters.py
+++ parameters.py
@@ -16,11 +16,24 @@ M_MTOW = 12000 # [kg]
 CL_MAX_CR = 1.6 # Cruise
 CL_MAX_TO = 1.9 # Takeoff
 CL_MAX_L = 2.1 # Landing
+AR = 5 # Aspect ratio
+CD_0 = 0.2 # CD0
 
 C_LFL = 0.45 # FAR 25 0.6 for 23
 V_APP = 60 # [m/s] estimate
 
+B = 5 # Bypass ratio, assumed
+THETA_BREAK = 1.07 # assumed
+
 # ENVIRONMENTAL CONDITIONS
-RHO = 1.225 # [kg/m^3]
+RHO_SL = 1.225 # [kg/m^3]
+GAMMA = 1.4 # specific heat ratio in air
+
 n_v = 0.85
+
+T_SL = 288.15 # [K]
+P_SL = 101325 # [Pa]
+T_CR = 200
+P_CR = 50000
+
 
