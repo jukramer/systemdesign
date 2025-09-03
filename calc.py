56677ce6e95197cde7c35074616b5d95391f2211
jukramer <justas.kramer@gmail.com>
2025-09-03 13:52:43 +0200

    params 2

---
+++ calc.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from parameters import *
+
+class Calc:
+    def __init__(self):
+        pass
+    
+    def wingLoadingMax():
+        return None
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from parameters import *
+
+
+if __name__ == '__main__':
+    pass
