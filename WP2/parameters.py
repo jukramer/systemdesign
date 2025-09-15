f234f5bc6c346684f8f3abe3df9d1381cfddec23
kajuskan05-code <kajus.kan.05@gmail.com>
2025-09-15 15:46:58 +0200

    par

--- WP2/parameters.py
+++ WP2/parameters.py
@@ -4,8 +4,8 @@ b = 20 # [m]
 c_ROOT = 5 # [m]
 c_TIP = 2 # [m]
 
-LAMBDA_LE = 20 # [deg]
-LAMBDA_C4 = 30 # [deg]
+LAMBDA_LE = 8 # [deg]
+LAMBDA_C4 = 10.56 # [deg]
 LAMBDA_TE = 40 # [deg]
 
-TAPER_RATIO = 0.4 # [-]
+TAPER_RATIO = 0.363 # [-]
