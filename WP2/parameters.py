95716a589151313599aa7cf9934ec6dee3c0d58c
jukramer <justas.kramer@gmail.com>
2025-09-15 15:39:44 +0200

    MAC shit

--- WP2/parameters.py
+++ WP2/parameters.py
@@ -1,4 +1,11 @@
-S = 50 # [m^2], wing surface area
-b = 20 # [m], 
-c_ROOT = 5
-c_TIP = 2
+S = 50 # [m^2]
+b = 20 # [m]
+
+c_ROOT = 5 # [m]
+c_TIP = 2 # [m]
+
+LAMBDA_LE = 20 # [deg]
+LAMBDA_C4 = 30 # [deg]
+LAMBDA_TE = 40 # [deg]
+
+TAPER_RATIO = 0.4 # [-]
