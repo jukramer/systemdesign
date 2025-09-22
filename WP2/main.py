518eabf44a34de6a7f4c072acfe870c83a2855c8
jukramer <justas.kramer@gmail.com>
2025-09-22 14:12:47 +0200

    updates idk

--- WP2/main.py
+++ WP2/main.py
@@ -3,7 +3,8 @@ import numpy as np
 
 if __name__ == '__main__':
     INT_INTRVLS = 100 # integration intervals
-    # calc = Calc(INT_INTRVLS)
+    calc = Calc(INT_INTRVLS)
+    print(calc.calc_c())
     
-    print(b/2*np.tan(np.deg2rad(LAMBDA_LE)))
+    # print(b/2*np.tan(np.deg2rad(LAMBDA_LE)))
     
