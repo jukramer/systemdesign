f6d11cc4957f424c83eda78e652925b9d6deaf44
Zardous <m_goede@outlook.com>
2025-10-08 17:30:08 +0200

    updates

--- WP2/calc.py
+++ WP2/calc.py
@@ -17,56 +17,37 @@ class Calc():
     
     def mac(self):
         y_VALS = np.arange(0, b/2, self.INT_INTRVLS)
-        return 2/S*np.trapz(self.c(y_VALS), y_VALS)
+        return 2/S*np.trapz(self.c(y_VALS), y_VALS) #type:ignore
     
     def pos_mac(self):
         y = (b/6)*((1+2*TAPER_RATIO)/(1+TAPER_RATIO))
         x = y*np.tan(LAMBDA_LE)
 
         return x, y
-
-    # def descent(self):
-    #     q = 0.5 * 1.225 * (V**2) # [N/m^2]
-    #     CL_des = 1.1 / q *(0.5 * (WS_start - WS_end))
-    #     return CL_des
     
-    def airfoil_Cl(self):
-        Cl_airfoil = None
+    def check_aspect_ratio(self):
+        A_check = 4 / ((Cl_airfoil + 1)*cos(LAMBDA_LE))
+        if A_check < A:
+            return True
+        else:
+            return False
     
-    # def check_aspect_ratio(self):
-    #     A_check = 4 / ((Cl_airfoil + 1)*cos(LAMBDA_LE))
-    #     if A_check < A:
-    #         return True
-    #     else:
-    #         return False
+    def estimayte_MDD(self):
+        MDD = k_a / cos(LAMBDA) - tc_streamwise / cos(LAMBDA)**2 - CL / (10 * cos(LAMBDA)**3)
+        return MDD
     
-    # def estimayte_MDD(self):
-    #     MDD = k_a / cos(LAMBDA) - tc_streamwise / cos(LAMBDA)**2 - CL / (10 * cos(LAMBDA)**3)
-    #     return MDD
-    
-    # def wing_component_form_factor(self):
-    #     FF_w = (1 + 0.6 / x_c_m * tc + 100 * tc**4) * (1.34 * M**0.18 * cos(LAMBDA_LE)**0.28)
-    #     return FF_w
+    def wing_component_form_factor(self):
+        FF_w = (1 + 0.6 / x_c_m * tc + 100 * tc**4) * (1.34 * M**0.18 * cos(LAMBDA_LE)**0.28)
+        return FF_w
      
-    # def wave_drag(self):
-    #     if M < M_critical:
-    #         CD_wave = 0 
-    #     elif M >= M_critical and M <= MDD:
-    #         CD_wave = 0.002 * (1 + 2.5 * (MDD - M)/0.05)**(-1)
-    #     else:
-    #         CD_wave = 0.002 * (1 + 2.5 * (MDD - M)/0.05)**(2.5)
-    #     return CD_wave
-    
-    # def getM(self, polar_file, v_inf, alpha_cr):
-    #     polar = np.loadtxt(polar_file, skiprows=11)
-    #     c_p = polar[:,7]/np.sqrt(1-0.68**2)
-    #     alpha = polar[:,0]
-    #     v = np.sqrt(1-c_p)*v_inf
-    #     v *= np.cos(14.822*np.pi/180)
-    #     # print(np.concatenate((alpha[:, None], c_p[:, None]), axis=1).shape)
-    #     M = np.concatenate((alpha[:, None], v[:, None]/a_CRUISE), axis=1)
-    #     M_CR = np.max(np.where(np.abs(alpha_cr-alpha)==np.min(np.abs(alpha_cr-alpha)), v[:, None]/a_CRUISE, -100)).item()
-    #     return M_CR
+    def wave_drag(self):
+        if M < M_critical:
+            CD_wave = 0 
+        elif M >= M_critical and M <= MDD:
+            CD_wave = 0.002 * (1 + 2.5 * (MDD - M)/0.05)**(-1)
+        else:
+            CD_wave = 0.002 * (1 + 2.5 * (MDD - M)/0.05)**(2.5)
+        return CD_wave
     
     def getLD(self, polar_file):
         polar = np.loadtxt(polar_file, skiprows=11)
@@ -75,7 +56,6 @@ class Calc():
         cl_CR = 0.2643
 
         LD = cl/cd
-        # print(LD)
         LD_CR = np.max(np.where(np.abs(cl-cl_CR)==np.min(np.abs(cl-cl_CR)), LD, -100)).item()
         cd_min = np.min(cd)
         cl_cdmin = np.max(np.where(cd==cd_min, cl, -100)).item()
@@ -89,18 +69,18 @@ class Calc():
         c_p = polar[:,7]/np.sqrt(1-0.68**2)
         v = np.sqrt(1-c_p)*V_CRUISE
         v *= np.cos(14.822*np.pi/180)
-        M_CR = np.max(np.where(np.abs(cl-cl_CR)==np.min(np.abs(cl-cl_CR)), v/a_CRUISE, -100)).item()
+        M_local = np.max(np.where(np.abs(cl-cl_CR)==np.min(np.abs(cl-cl_CR)), v/a_CRUISE, -100)).item()
 
-        # print()
-        # print(abs(cm_CR))
-        # print(np.max(cl)*np.sqrt(1-0.68**2))
-        # print(np.max(np.where(cl == np.max(cl), polar[:,0], -100)))
-        # print(abs(alpha_cr/np.pi*180))
-        # print()
-        # print(M_CR)
-        # print(cd_min)
-        # print(delta_cl)
-        # print(LD_CR)
-        # print()
+        print()
+        print(abs(cm_CR))
+        print(np.max(cl)*np.sqrt(1-0.68**2))
+        print(np.max(np.where(cl == np.max(cl), polar[:,0], -100)))
+        print(abs(alpha_cr/np.pi*180))
+        print()
+        print(M_local)
+        print(cd_min)
+        print(delta_cl)
+        print(LD_CR)
+        print()
         print(alpha_0cl)
         return alpha_cr
