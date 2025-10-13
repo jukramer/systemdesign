baa5d9423ffcc26c5b09a0cadd823494517e9280
jettrimborn-creator <jet.trimborn@gmail.com>
2025-10-13 14:37:48 +0200

    updates

--- WP3/3.3/wettedarea.py
+++ WP3/3.3/wettedarea.py
@@ -1,10 +1,28 @@
-thickness = 0
-Sexp = 0 
-Atop = 0
-Aside = 0
-K = 3.4
 
-Swet_wing = Sexp* (1.977+0.53*(thickness))
-Swet_fus = K*(Atop +Aside)/2
+import math as m
 
-S_wet = Swet_wing + Swet_fus 
+D_fus = 0
+L1 = 0
+L2 = 0
+L3 = 0
+S_wexp = 0
+S_hexp = 0
+S_vexp = 0
+
+
+
+
+Swet_wing = 1.07 * 2 * S_wexp
+Swet_fus = ((m.pi * D_fus)/4)(1/(3*L1^2)((4*L1^2+D_fus^2/4)^1.5-(D_fus^3/8))-D_fus+4*L2+2*m.sqrt(L3^2+D_fus^2/4))
+Swet_hor = 1.05 * 2 * S_hexp
+Swet_vert = 1.05 * 2 * S_vexp
+
+
+
+print()
+
+
+S_wet = Swet_wing + Swet_fus + Swet_hor + Swet_vert
+print(S_wet)
+
+
