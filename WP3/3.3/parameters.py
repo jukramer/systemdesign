c440cb772d7ac1d6dab7118fe33b190c0acb192b
lauraverduijn <laura_verduijn@hotmail.com>
2025-10-20 15:45:58 +0200

    update

--- WP3/3.3/parameters.py
+++ WP3/3.3/parameters.py
@@ -40,28 +40,28 @@ w_LG = 0 # width landing gear [m]
 
 
 # Weight
-g = 9.80665         # gravitational acceleration [m/s^2]
-lbs_to_N = 4.448    # pound to newton conversion
-m_to_ft = 3.280839895   # meter to feet conversion
-W_TO = 9907.7*g     # maximum take off weight [N]
-W_F = 2960*g        # fuel weight [N]
-W_MZF = (W_TO - W_F)/lbs_to_N  # maximum zero fuel weight
-b = 20.435*m_to_ft          # span [ft]
-Lambda_12 = 0       # wing semi-chord sweep angle [rad]
-S = 46.919*m_to_ft**2          # wing area [m^2]
-n_ult = 4.275       # ultimate load factor [-]
-t_r = 0             # maximum root thickness [m]
+g = 9.80665                     # gravitational acceleration [m/s^2]
+lbs_to_N = 4.448                # pound to newton conversion
+m_to_ft = 3.280839895           # meter to feet conversion
+W_TO = 9907.7*g                 # maximum take off weight [N]
+W_F = 2960*g                    # fuel weight [N]
+W_MZF = (W_TO - W_F)/lbs_to_N   # maximum zero fuel weight
+b = 20.435*m_to_ft              # span [ft]
+Lambda_12 = 0                   # wing semi-chord sweep angle [rad]
+S = 46.919*m_to_ft**2           # wing area [ft^2]
+n_ult = 4.275                   # ultimate load factor [-]
+t_r = 0                         # maximum root thickness [ft]
 
-k_h = 1.0           # for fixed incidence stabilizers [-]
-S_h = 0             # horizontal tail area [m^2]
-V_D = 0             # design dive speed [m/s]
-Lambda_12_h = 0     # semi-chord sweep angle horizontal tail [rad]
+k_h = 1.0                       # for fixed incidence stabilizers [-]
+S_h = 0                         # horizontal tail area [ft^2]
+V_D = 0                         # design dive speed [KEAS]
+Lambda_12_h = 0                 # semi-chord sweep angle horizontal tail [rad]
 
-S_v = 0             # vertical tail area [m^2]
-z_h = 0             # distance from vert.tail root to where ht is mounted on the v.t.
-b_v = 0             # vertical tail span [m]
+S_v = 0                         # vertical tail area [ft^2]
+z_h = 0                         # distance from vert.tail root to where ht is mounted on the v.t. [ft]
+b_v = 0                         # vertical tail span [ft]
 k_v = 1 + 0.15*(S_h*z_h/(S_v*b_v))         # for fuselage mounted horizontal tails
-Lambda_12_v = 0     # semi-chord sweep angle horizontal tail [rad]
+Lambda_12_v = 0                 # semi-chord sweep angle horizontal tail [rad]
 
 k_f = 1.08          # for a pressurized fuselage
 V_D = 0             # design dive speed [KEAS]
@@ -72,16 +72,16 @@ S_fgs = 0           # fuselage gross shell area [ft]
 
 T_TO = 0            # total required take-off thrust
 
-K_gr = 1.0          # for low wing airplanes
-A_g_main = 33.0
-B_g_main = 0.04
-C_g_main = 0.021
-D_g_main = 0.0
-A_g_nose = 12.0
-B_g_nose = 0.06
-C_g_nose = 0.0
-D_g_nose = 0.0
-K_fc = 0.64         # constant for airplanes with powerd flight controls
+K_gr = 1.0          # for low wing airplanes [-]
+A_g_main = 33.0     # constant for land gear main [-]
+B_g_main = 0.04     # constant for land gear main [-]
+C_g_main = 0.021    # constant for land gear main [-]
+D_g_main = 0.0      # constant for land gear main [-]
+A_g_nose = 12.0     # constant for land gear nose [-]
+B_g_nose = 0.06     # constant for land gear nose [-]
+C_g_nose = 0.0      # constant for land gear nose [-]
+D_g_nose = 0.0      # constant for land gear nose [-]
+K_fc = 0.64         # constant for airplanes with powerd flight controls [-]
 V_pax = 0           # passenger cabin volime in [ft^3]
 R = 0               # range in nautical miles
 W_E = 0             # empty weight in[lbs]
