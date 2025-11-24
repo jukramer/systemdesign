c8385be08eb74277babe8a24107f03a7a1fbf400
Patricio-LetiFer <pletipichiafer@tudelft.nl>
2025-11-24 14:54:05 +0100

    Moment diagram

--- WP4/4.1/Diagrams.py
+++ WP4/4.1/Diagrams.py
@@ -5,19 +5,24 @@ import numpy as np
 
 b_w = 20.4
 L = b_w / 2
-x = 0.0
-dx = 0.1
 
 # Moment diagram
 
-x_vals = np.arange(0, L, 0.1)
-y_vals = np.sin(x_vals)
+z_vals = np.arange(0, L, 0.1)
+y_vals = np.sin(z_vals)
+line = np.zeros_like(z_vals)
 
 # Graph
-plt.plot(x_vals, y_vals)
+plt.plot(z_vals, y_vals, label='Moment', color='red')
+plt.plot(z_vals, line, linestyle='-', color='black')
 plt.title('Moment Diagram')
-plt.xlabel('x')
-plt.ylabel('y')
+plt.xlabel('z [m]')
+plt.ylabel('M [Nm]')
 plt.grid(True)
+plt.xlim(0,L)
 
+plt.legend()
 plt.show()
+
+
+
