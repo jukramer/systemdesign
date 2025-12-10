1c403bc1688ea33437a17c0a86c978026d9cbea8
Zardous <m_goede@outlook.com>
2025-12-10 15:50:45 +0100

    typo

--- WP5/FM_skin_shear/calc.py
+++ WP5/FM_skin_shear/calc.py
@@ -4,8 +4,8 @@ import os, sys
 from parameters import *
 
 current_folder = sys.path[0]
-WP5 = os.path.dirname(os.path.dirname(current_folder))
-sys.path.append(WP5)
+sys_design_folder = os.path.dirname(os.path.dirname(current_folder))
+sys.path.append(sys_design_folder)
 from WP4.WP4_2 import Beam
 
 def skin_shear_buckling(k_s, t, b):
