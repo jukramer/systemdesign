ad6de752316db43ed615356387ae9b94d95efff1
jukramer <justas.kramer@gmail.com>
2025-12-10 15:52:19 +0100

    updates

--- WP5/Skin Buckling/calc.py
+++ WP5/Skin Buckling/calc.py
@@ -3,6 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
-def skinBucklingStress(t, b):
-    return np.pi**2*kC*E / (12*(1-POISSION_RATIO**2)) * (t/b)**2
+class Calc:
+    def __init__(self):
+        return
 
+    def skinBucklingStress(t, b):
+        return np.pi**2*kC*E / (12*(1-POISSION_RATIO**2)) * (t/b)**2
+
+
+if __name__ == '__main__':
+    pass
