107bc50095f5803b88a36e4d0498e731cb69c163
MaddyHatter13 <aashnapatel05@gmail.com>
2025-12-15 14:09:58 +0100

    column buckling

--- WP5/Column Buckling/Column_buckling.py
+++ WP5/Column Buckling/Column_buckling.py
@@ -1,5 +1,6 @@
 import numpy as np
 from parameters import *
 
-
-sigme_crit = (K * np.pi**2 * E * I)/(L**2 * A)
+def sigma_crit(K, E, I, L, A):
+    sigme_crit = (K * np.pi**2 * E * I)/(L**2 * A)
+    return sigma_crit
