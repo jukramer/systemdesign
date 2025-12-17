73b77189e1ce537fa44671469f643763aa5623fc
jukramer <justas.kramer@gmail.com>
2025-12-17 15:23:42 +0100

    updates

--- WP5/Beam.py
+++ WP5/Beam.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy as sp
 
+
 class Beam():
     def __init__(self, intg_points: int = 100) -> None:
         self.intg_points = intg_points
