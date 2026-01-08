473335e3924463503c432ba73ddaacfebe503bfb
jukramer <justas.kramer@gmail.com>
2026-01-08 15:54:58 +0100

    updates to plotting

--- WP5/Test.py
+++ WP5/Test.py
@@ -1,14 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
-import scipy as sp
 
-
-x=[0.7,0.85,1.0,1.15,1.3,1.45,1.6,1.8,2.0,2.2,2.4,2.6,2.75,2.9,3.05,3.2,3.35,3.5,3.7,3.85,4.0,4.15,4.3,4.45,4.6,4.8,5.0]
-y=[10.7,7.7,6.7,6,5.6,5.5,5.5,5.1,4.8,4.5,4.4,4.5,4.6,4.5,4.4,4.4,4.4,4.3,4.3,4.3,4.2,4.2,4.2,4.1,4.1,4.1,4.1]
-f=sp.interpolate.interp1d(x,y,kind='cubic')
-xnew=np.arange(np.min(x),np.max(x),0.001)
-ynew=f(xnew)
-plt.plot(x,y,'o',xnew,ynew,'-')
-plt.show()
-
-
+print(np.linspace(20, 5, 9))
--- WP5/plotSafetyMargin.py
+++ WP5/plotSafetyMargin.py
@@ -26,62 +26,25 @@ class DimensionError(Exception):
 
 
 # y values, raw applied stress values, failure stress, safety factor for applied stress
-def plotFailureMargin(yVals: NDArray, sigmaAppliedVals: NDArray, sigmaFail: float, n: float, dimSubplots: tuple, titles: tuple=(), colors: tuple=()) -> None:
+def plotFailureMargin(yVals: NDArray, sigmaAppliedVals: NDArray, sigmaFail: NDArray, n: float) -> None:
     # Check for correct parameter dimensions/types
     if yVals.shape != sigmaAppliedVals.shape:
         raise DimensionError('yVals and sigmaAppliedVals must have same length!')
-    if not isinstance(dimSubplots, tuple):
-        raise TypeError('dimSubplots, titles must be tuples!')
-        
-    # Default values for colors
-    if len(colors) == 0:
-        colors = (int(yVals.shape[1]) * 'blue') 
-        
-    # To allow for inhomogeneous y/sigma arrays (plots with different # of data points),
-    # numpy arrays are unpacked into python lists
-    yArrays = [np.array(yVals[i]) for i in range(yVals.shape[0])]
-    sigmaAppliedArrays = [np.array(yVals[i]) for i in range(yVals.shape[0])]
-        
-    # Array of failure stress values to allow for array division
-    sigmaFailArrays = [n*np.full_like(sigmaAppliedArray, sigmaFail) for sigmaAppliedArray in sigmaAppliedArrays] 
-    marginOfSafetyArrays = [sigmaFailArray/sigmaAppliedArray for sigmaFailArray, sigmaAppliedArray in zip(sigmaFailArrays, sigmaAppliedArrays)]
-    
-    # Plotting
-    fig, axs = plt.subplots(*dimSubplots)
-    if isinstance(axs[0,:], np.ndarray):
-        axs = axs.ravel()
-    
-    for i, ax in enumerate(axs):
-        # Handle unspecified colors/plots/titles
-        try:
-            ax.plot(yArrays[i], marginOfSafetyArrays[i], color=colors[i])
-        except IndexError:
-            try:
-                ax.plot(yArrays[i], marginOfSafetyArrays[i], color='blue')
-                print(f'Color for plot {i} not specified.')
                 
-            except IndexError:
-                ax.set_axis_off()
-        
-        try:
-            ax.set_title(titles[i])
-        except IndexError:
-            ax.set_title('')
+
+    # Array of failure stress values to allow for array division
+    safetyMarginArray = sigmaFail/sigmaAppliedVals
+    
+    ax, fig = plt.subplots(1, 1)        
             
-        ax.set_xlabel('y [m]')
-        ax.set_ylabel('Margin of Safety [-]')
-        ax.grid()
+    ax.set_xlabel('y [m]')
+    ax.set_ylabel('Margin of Safety [-]')
+    ax.grid()
                 
     fig.tight_layout()
-    fig.suptitle('Margins of Safety')
+    fig.suptitle('Margins of Safety *failure mode*')
     plt.show()
     
 
 if __name__ == '__main__':
-    plotFailureMargin(np.array([[1,2,3], [1,2], [2,4,5,6,7]], dtype=object), 
-                      np.array([[2,4,1], [3,5], [9,3,4,5,6]], dtype=object), 
-                      10,
-                      1.5,
-                      (2,2),
-                      ('Doiuglaes', 'Test'),
-                      ('red', 'blue'))
+    pass
